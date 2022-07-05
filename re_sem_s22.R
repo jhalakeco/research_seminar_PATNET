20409729dce5c2af6d8dc8a008419788ef1fb8e4
Jhalak Gope <jhalak.eco@gmail.com>
2022-07-05 05:59:42 +0200

    - overall documentation cleaned - graphs added - perioodical analysis for top five cities initialised - ndtv packaged used

--- re_sem_s22.R
+++ re_sem_s22.R
@@ -8,9 +8,7 @@ library(network)
 library(networkD3)
 library(sna)
 library(intergraph)
-library(sp)
 library(RColorBrewer)
-library(data.table)
 
 # loading datasets for wind energy IPC3, inventors and regions
 load("res_sem.RData")
@@ -22,8 +20,6 @@ data_final <- cbind(joined_IPC_Inv, IPC4) # combining the newly created IPC3 con
 rm(joined_IPC_Inv) & rm(IPC4) # removing the dataset to free memory
 data_final <- subset(data_final, select = -c(IPC,ctry_code)) # dropping columns
 
-
-colnames(data_final)
 
 
 # Data cleaning ----
@@ -161,7 +157,7 @@ inv_person_adj <- inv_person_2mode %*% t(inv_person_2mode)
 dim(inv_person_adj)
 class(inv_person_adj)
 
-inv_person_adj <- diag.remove(inv_person_adj, remove.val = 0)
+
 inv_person_nw <- network(inv_person_adj,
                          directed = F)
 par(mar=c(0,0,0,0))
@@ -183,10 +179,12 @@ class(inv_aff_all)
 
 ### A1 plotting ----
 set.seed(100)
+par(mar=c(0,0,1,0))
 plot(inv_aff_ig,
      layout = layout.auto(inv_aff_ig),
      vertex.size = 2,
-     vertex.label = NA)
+     vertex.label = NA,
+     main="Network of all the inventors")
 
 
 ## A2. Converting the network package object into igraph object (for 15 cities)----
@@ -196,24 +194,23 @@ class(inv_city_ig)
 inv_city_ig <- (inv_city_ig %>% 
                   set_vertex_attr(
                     name = "vertex.names",
-                    value = c("Aurich","Hamburg","Berlin","Salzbergen","Rendsburg","Rheine","Kiel","Aachen","Munchen","Norderstedt","Osnabruck","Bremen","Erlangen","Dresden","Munster")
+                    value = c("Aurich","Hamburg","Berlin","Salzbergen","Rendsburg","Rheine","Kiel","Aachen","Munchen","Norderstedt","Osnabruck","Bremen","Erlangen","Dresden","Munster") # modifying vertex names on the igraph object (as city names)
                   ))
-V(inv_city_ig)$label <- c("Aurich","Hamburg","Berlin","Salzbergen","Rendsburg","Rheine","Kiel","Aachen","Munchen","Norderstedt","Osnabruck","Bremen","Erlangen","Dresden","Munster")
+V(inv_city_ig)$label <- c("Aurich","Hamburg","Berlin","Salzbergen","Rendsburg","Rheine","Kiel","Aachen","Munchen","Norderstedt","Osnabruck","Bremen","Erlangen","Dresden","Munster") # changing the igraph object's labels also into the city names
 
 
 ### A2 plotting ----
+par(mar=c(0,0,2,0))
 set.seed(100)
-par(mar=c(0,0,1,0))
 plot(inv_city_ig,
      vertex.label=V(inv_city_ig)$city,
      vertex.color=V(inv_city_ig),
      vertex.size= degree(inv_city_ig)*3,
-     main="City network according to their density")
+     vertex.frame.color=NA,
+     vertex.label.family= "Cambria",
+     vertex.label.font=2,
+     main="Network of top 15 cities \n (according to their density)")
 
-eb_inv_city_ig <- cluster_edge_betweenness(inv_city_ig) # calculating betweeness
-plot(eb_inv_city_ig,
-     inv_city_ig,
-     vertex.size=2,)
 #### A2 Centrality measurements----
 centr_degree(
   inv_city_ig,
@@ -228,72 +225,66 @@ class(inv_person_ig)
 set_vertex_attr(graph = inv_person_ig, name = "city", value = "city")
 
 ### A3 plotting ----
-eb_inv_person_ig <- cluster_edge_betweenness(inv_person_ig)
+#### the network graph  ----
+par(mar=c(0,0,1,0))
+set.seed(100)
 plot(inv_person_ig,
      vertex.label=NA,
+     vertex.frame.color=NA,
      vertex.color=V(inv_person_ig),
-     vertex.size=3)
-     #vertex.label= (inv_per_attr$city),
+     vertex.size=2,
+     edge.colour="black",
+     main="Inventor's network for top 15 cities",
+     sub=("(on edge betweenness of the inventors)"))
+
+#### plot with edge betweenness ----
+par(mar=c(0,0,1,0))
+plot(inv_person_ig,
+     vertex.label=NA,
+     vertex.frame.color=NA,
+     vertex.color=V(inv_person_ig),
+     vertex.size=2,
+     edge.curved=.1,
+     main="Inventor's network for top 15 cities",
+     sub=("(on edge betweenness of the inventors)"),
+     edge.width = edge_betweenness(inv_person_ig))
      
 plot(inv_person_ig,
      vertex.label = ifelse(degree(inv_person_ig) >= .01, V(inv_person_ig)$label, NA))
 
-
-
+##### A3 centrality measurements ----
+centr_degree(
+  inv_person_ig,
+  mode = c("all", "out", "in", "total"),
+  loops = F,
+  normalized = TRUE)
 
 
 # Top 5 city's innovation scenario over the time ----
 ## top 5 city, year 2005 and 2015
 
+detach(package:igraph)
+library(network)
+library(ndtv)
+
 ### Aurich ----
+aurich_d_2007 <- (data_main_15_attr %>% 
+                    filter(city=="Aurich", app_year==2007) %>% 
+                    select(city, appln_id, inv_name))
+
+aurich_d_2016 <- (data_main_15_attr %>%
+                    filter(city=="Aurich", app_year==2016) %>%
+                    select(inv_name, appln_id))
+aurich_d_all <- (data_main_15_attr %>% 
+                   filter(city=="Aurich") %>% 
+                   select(appln_id, app_year))
 
 
 
 
 
-
-
-# igraph and igraph data ----
-
-library(intergraph) # Dr. Graf
-inv_city_ig <- asIgraph(inv_city_nw) # Dr. Graf
-class(inv_city_ig)
-
-
-
-
-# plotting graph for igraph
-igraph.options(vertex.size = 10)
-plot(inv_city_ig,
-     layout = layout.drl(inv_city_ig),
-     vertex.label = inv_city_ig,
-     edge.arrow.size=.8,
-     edge.color="gray",
-     )
-
-
-
-
-city_pat <- data.frame(city = names(diag(inv_city_adj)), pat = diag(inv_city_adj)) # Dr.Graf
-city_pat
-
-city_net_extra <- network(inv_cityD3)
-plot.network(city_net_extra,
-             label = network.vertex.names(inv_city_nw),
-             interactive =T)
-
-
-
-# NetworkD3 ----
-
-
-library(networkD3)
 par(mar=c(0,0,0,0))
-simpleNetwork(inv_cityD3,
-              fontSize = 12,
-              opacity = 10,
-              zoom = TRUE)
-
+simpleNetwork(aurich_d_2016)
 
 ##### Network measurements
 
@@ -322,16 +313,12 @@ plot.network(invp_nw, displayisolates = F, edge.lwd = get.edge.value(invp_nw,"st
 gden(inv_city_nw)
 
 
-#--------- igraph -------
 
-inv_nw_igraph <- graph_from_data_frame(inv_person, "undirected", F)
-igraph.options(vertex.size = 3,
-               edge.arrow.size = 0.2,
-               vertex.label = NA,
-               vertex.color ="black")
-plot(inv_nw_igraph)
+# From Dr. Graf :: igraph and igraph data ----
 
-
+library(intergraph) # Dr. Graf
+inv_city_ig <- asIgraph(inv_city_nw) # Dr. Graf
+class(inv_city_ig)
 
 
 
